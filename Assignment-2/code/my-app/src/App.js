47f2a9383ac452e6918757d2c6d878a6879da3a0
Mohamed Jamesha <mohamedjamesha25@gmail.com>
2022-03-31 10:44:11 +0530

    assignment 2 submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -5,11 +5,26 @@ import './App.css';
 
 function App(props) {
  const {employee}=props;
-    
+  
+ const renderFields=(label,value)=>{
+   return(
+     <div className='fields-container'>
+       <label>{label}</label>
+       <div>{value}</div>
+     </div>
+   );
+ }
+
 return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
+  <div className="App">
+    <div className='card-container'>
+      <img src = {employee.profileImg} width='100%'/>
+      <h2>{employee.name}</h2>
+      {renderFields('location',employee.location)}
+      {renderFields('bloodGroup',employee.bloodGroup)}
+      {renderFields('age',employee.age)}
+    </div>
+  </div>
 )
 }
 

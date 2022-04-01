64bf62ffdaea5ee0b74eed7b23d8d963cd493f59
Mohamed Jamesha <mohamedjamesha25@gmail.com>
2022-04-01 16:47:17 +0530

    assignment 3 submission

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
-var arrow="<"
+
 function App() {
+  const renderFields=(label,value)=>{
+    return(
+      <div className='field-container'>
+        <span className='label'>{label}</span>
+        <span className='value'>{value}</span>
+      </div>
+    );
+  }
   return (
     <div className="site-container">
-    {/* Start Your code here */}
+      <div className='header'>
+        <span className='btn'>&lt;</span>
+        <span>Alan Ford <br/>00005152</span>
+        <span className='btn'>Print</span>
+      </div>
+      <div className='customer-info'>
+        {renderFields('Appointment:','9:00 (25-05-2016)')}
+        {renderFields('Email:','jamy@gmail.com')}
+        {renderFields('Phone:','+9629002859')}
+      </div>
+      <div className='order-info'>
+        {renderFields('Status','In Progress')}
+        {renderFields('Door','Mark')}
+        {renderFields('Time','9:00 (25-05-2016)')}
+      </div>
+      <div className='product-list'>
+        <span className='checkBox'><input type='checkbox'/></span>
+        <img src='https://www.w3schools.com/howto/img_avatar.png' width='200px'/>
+        {renderFields('Harry Potter','Hello, my name is potter, Harry Potter. I make pots.')}
+      </div>
     </div>
   );
 }
